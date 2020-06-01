7babeb96dbce7f540768b12b197e42f7ad9c38a0
Maxim Palotti <41835828+MyHouseIsBig@users.noreply.github.com>
2020-06-01 12:04:54 +0200

    first commit

---
+++ src/components/pages/homepage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Homepage() {
+  return (
+    <p>Home Nigger</p>
+  );
+}
+
+export default Homepage;
---
+++ src/components/pages/newpage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function NewPage() {
+  return (
+    <p>NewPage Nigger</p>
+  );
+}
+
+export default NewPage;
